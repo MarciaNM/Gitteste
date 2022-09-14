1f155dd96a899d0d33020fb574aeee940fbea188
Márcia Nardes <marcianarddes@gmail.com>
2022-09-14 13:15:50 -0300

    aletrações

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,10 +1,14 @@
-import { gql } from 'apollo-server',
+import { gql } from 'apollo-server';
 const rootTypeDefs = gql`
   type Query {
-  hi: String
+    hi: String
   }
 `;
-
+const rootResolvers = {
+  Query: {
+    hi: () => 'hi again',
+  },
+};
 
 export const typeDefs = [rootTypeDefs];
-export const resolvers = [];
+export const resolvers = [rootResolvers];
