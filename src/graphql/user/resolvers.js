7bdbdb0f5529db61d4b381e80e132237cfcbca99
Márcia Nardes <marcianarddes@gmail.com>
2022-09-15 16:47:47 -0300

    inclusão do fetch aula 21 e teste

--- src/graphql/user/resolvers.js
+++ src/graphql/user/resolvers.js
@@ -1,9 +1,15 @@
 import fetch from 'node-fetch';
 
-const users = async (_, __, { fetch }) => {
+const user = async (_, __, { fetch }) => {
   console.log(fetch);
   const users = await fetch('http://localhost:3000/users');
   return users.json();
+};
+
+const users = async (_, __, { fetch }) => {
+  console.log(fetch);
+  const user = await fetch('http://localhost:3000/users/602');
+  return user.json();
 };
 
 const user = async () => {
