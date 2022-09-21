e26ad872894d2a7a4f5a9a96d82aa702c23ad7d7
Márcia Nardes <marcianarddes@gmail.com>
2022-09-21 15:30:00 -0300

    aula 29 criado pasta api-filter

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -2,7 +2,9 @@ const post = async (_, { id }, { getPosts }) => {
   const response = await getPosts('/' + id);
   return response.json();
 };
-const posts = async (_, __, { getPosts }) => {
+const posts = async (_, { input }, { getPosts }) => {
+  const apiFiltersInput = new URLSearchParams(input);
+  console.log(apiFiltersInput.toString());
   const response = await getPosts();
   return response.json();
 };
