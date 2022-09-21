60292d6d65ace07aaca044679fbb355e92022275
Márcia Nardes <marcianarddes@gmail.com>
2022-09-21 09:47:48 -0300

    aula 26 inclusão de uma consulta no post

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -8,8 +8,18 @@ const posts = async (_, __, { getPosts }) => {
 };
 
 export const postResolvers = {
-  Query: {
-    post,
-    posts,
+  Query: { post, posts },
+  Post: {
+    unixTimestamp: ({ createdAT }) => {
+      const timestamp = new Date(createdAT).getTime() / 1000;
+      return Math.floor(timestamp);
+    },
   },
 };
+
+/*esse post não é da query e sim resolver resolvendo depois do post
+foi testado este campo da base e modificado a data para ver em milisegundos o valor
+getTime()/1000:dividi em segundos arrendonda para baixo
+e para validar pode buscar os segundos no site epochconverter.com e confere com a data que está no banco de dados,
+pode dar uma diferença na consulta por que o GMT pode dar pouca diferença
+*/
--- src/graphql/post/typedefs.js
+++ src/graphql/post/typedefs.js
@@ -13,5 +13,6 @@ export const postTypeDefs = gql`
     #userId (user: USer!)
     indexRef: Int!
     createdAt: String!
+    unixTimestamp: String! #Criado esse campo para testar no resolvers,um campo há mais que não tinha na base de dados
   }
 `;
