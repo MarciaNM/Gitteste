2f54570610e3d08dec9d2f9dd46947758cd58012
Márcia Nardes <marcianarddes@gmail.com>
2022-09-22 08:11:35 -0300

    inclusão da queries_0010

--- src/graphql/api-filters/typedefs.js
+++ src/graphql/api-filters/typedefs.js
@@ -3,7 +3,7 @@ import { gql } from 'apollo-server';
 export const apiFiltersTypeDefs = gql`
   input ApiFiltersInput {
     _sort: String
-    _order: String
+    _order: ApiFilterOrder
     _start: Int
     _limit: Int
   }
