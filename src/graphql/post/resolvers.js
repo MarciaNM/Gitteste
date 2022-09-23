c3a819ed7df18bc1707ba55b31a40101beee6e0d
Márcia Nardes <marcianarddes@gmail.com>
2022-09-23 10:28:27 -0300

    aula 36

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,3 +1,6 @@
+import DataLoader from 'dataloader'; // baixado e importado dataloader aula 36
+import fetch from 'node-fetch'; // aula 36
+
 const post = async (_, { id }, { getPosts }) => {
   const response = await getPosts('/' + id);
   const post = await response.json(); //alterado para aula 31 para tratar o if abaixo
@@ -8,10 +11,22 @@ const posts = async (_, { input }, { getPosts }) => {
   const response = await getPosts('/?' + ApiFiltersInput);
   return response.json();
 };
+// aula 36 userDataloader
+const userDataloader = new DataLoader(async (ids) => {
+  // 1&id=2&id=3&id exemplo do array
+  const urlQuery = ids.join('&id='); //criando url com os id na ordem
+  const url = 'http://localhost:3000/users/?id=' + urlQuery;
+  const response = await fetch(url);
+  const users = await response.json();
+  //console.log(users); // para mostrar no terminal os id se quiser
+  //console.log(url); // para mostrar no terminal a url se quiser
+  return ids.map((id) => users.find((user) => user.id == id)); // mapea os id e vai dentro do array do id e vai nos id
+});
+
 const user = async ({ userId }, _, { getUsers }) => {
-  const response = await getUsers('/' + userId);
-  return response.json();
+  return userDataloader.load(userId);
 };
+
 export const postResolvers = {
   Query: { post, posts },
   Post: { user }, // criado aula 34 e tratado no user acima
