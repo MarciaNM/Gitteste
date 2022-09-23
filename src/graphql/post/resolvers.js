d9da11b0e5096675f7ab9deaab8b4fd3ef772130
Márcia Nardes <marcianarddes@gmail.com>
2022-09-23 10:53:06 -0300

    querie_0015 aula 36

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -13,16 +13,16 @@ const posts = async (_, { input }, { getPosts }) => {
 };
 // aula 36 userDataloader
 const userDataloader = new DataLoader(async (ids) => {
-  // 1&id=2&id=3&id exemplo do array
   const urlQuery = ids.join('&id='); //criando url com os id na ordem
   const url = 'http://localhost:3000/users/?id=' + urlQuery;
   const response = await fetch(url);
   const users = await response.json();
   //console.log(users); // para mostrar no terminal os id se quiser
   //console.log(url); // para mostrar no terminal a url se quiser
-  return ids.map((id) => users.find((user) => user.id == id)); // mapea os id e vai dentro do array do id e vai nos id
+  return ids.map((id) => users.find((user) => user.id == id));
 });
 
+// eslint-disable-next-line no-unused-vars
 const user = async ({ userId }, _, { getUsers }) => {
   return userDataloader.load(userId);
 };
