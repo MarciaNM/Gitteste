16cc835ec89a96c5f5066743334273e6ecd9c3a2
Márcia Nardes <marcianarddes@gmail.com>
2022-09-26 16:13:25 -0300

    alteração

--- src/graphql/user/context.js
+++ src/graphql/user/context.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
-import { getPosts } from './post/utils';
-import { getUsers } from './user/utils';
+import { getPosts } from '../post/utils';
+import { getUsers } from '../user/utils';
 import { makeUserDataLoader } from './user/dataloaders';
 import { makePostDataLoader } from './post/dataloaders';
 
