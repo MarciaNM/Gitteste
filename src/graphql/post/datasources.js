f9c0dc34a9122638c0605731dd7ecde57be69d93
Márcia Nardes <marcianarddes@gmail.com>
2022-09-27 08:55:20 -0300

    alteração aula 40

--- src/graphql/post/datasources.js
+++ src/graphql/post/datasources.js
@@ -1,9 +1,9 @@
 import { RESTDataSource } from 'apollo-datasource-rest';
 
-export class PostsApi RESTDataSource {
-  constructor(){
+export class PostsApi extends RESTDataSource {
+  constructor() {
     super();
-    this.baseURL = process.env API_URL + '/posts';
+    this.baseURL = process.env.API_URL + '/posts';
   }
   async gePosts(urlParams = {}) {
     return this.get('', urlParams);
