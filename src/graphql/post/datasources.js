70333cd8dda5b763985a1dc025a76073cd6e8035
Márcia Nardes <marcianarddes@gmail.com>
2022-09-27 13:34:09 -0300

    teste 2

--- src/graphql/post/datasources.js
+++ src/graphql/post/datasources.js
@@ -1,12 +1,14 @@
 import { RESTDataSource } from 'apollo-datasource-rest';
+// eslint-disable-next-line no-unused-vars
+import { URLSearchParams } from 'url';
 
 export class PostsApi extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = process.env.API_URL + '/posts/';
   }
-  async getPosts(URLParams = {}) {
-    return this.get('', URLParams, {
+  async getPosts(URLSearchParams = {}) {
+    return this.get('', URLSearchParams, {
       cacheOptions: { ttl: 60 },
     });
   }
