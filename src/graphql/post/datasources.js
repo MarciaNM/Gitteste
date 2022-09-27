5fa2e984a204d3b233b01e9125794c9f131c5931
Márcia Nardes <marcianarddes@gmail.com>
2022-09-27 09:45:48 -0300

    teste aula 40

--- src/graphql/post/datasources.js
+++ src/graphql/post/datasources.js
@@ -3,7 +3,7 @@ import { RESTDataSource } from 'apollo-datasource-rest';
 export class PostsApi extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = process.env.API_URL + '/posts';
+    this.baseURL = process.env.API_URL + '/posts/';
   }
   async gePosts(urlParams = {}) {
     return this.get('', urlParams);
--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,11 +1,10 @@
 const post = async (_, { id }, { dataSources }) => {
-  const post = dataSources.postApi.getPost(id);
+  const post = dataSources.postApi.gePost(id);
   return post;
 };
-const posts = async (_, { input }, { getPosts }) => {
-  const ApiFiltersInput = new URLSearchParams(input);
-  const response = await getPosts('/?' + ApiFiltersInput);
-  return response.json();
+const posts = async (_, { input }, { dataSources }) => {
+  const posts = dataSources.postApi.gePosts(input);
+  return posts;
 };
 
 const user = async ({ userId }, _, { userDataloader }) => {
