766cd7f1cbae6f0cbe100cea2bf295e7dd4f4ece
Márcia Nardes <marcianarddes@gmail.com>
2022-09-28 15:24:27 -0300

    alterado arquivo db.json

--- rest-methods.js
+++ rest-methods.js
@@ -1,22 +1,20 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
 import fetch from 'node-fetch';
-import { URLSearchParams } from 'url';
 const API_URL = process.env.API_URL;
 
-const get = (endPoint, urlParam, requestInit = {}) => {
-  return fetch(
-    API_URL + '/' + endPoint + '?' + new URLSearchParams(urlParam).toString(),
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      ...requestInit,
+const get = (endPoint, urlParam, requestInit) => {
+  const url =
+    API_URL + '/' + endPoint + '?' + new URLSearchParams(urlParam).toString();
+  return fetch(url, {
+    method: 'GET',
+    Headers: {
+      'Context-Type': 'application/json',
     },
-  );
+    ...requestInit,
+  });
 };
 (async () => {
-  const userGetResponse = await get('/users');
+  const userGetResponse = await get('users');
   const user = await userGetResponse.json();
-  console.log(user);
+  console.log();
 })();
