9200c5f84ab416f94fc7ca7a450ffa2bf9b6c165
Márcia Nardes <marcianarddes@gmail.com>
2022-09-28 12:23:40 -0300

    arquivo methods

---
+++ methods.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import fetch from 'node-fetch';
+import { URLSearchParams } from 'url';
+const API_URL = process.env.API_URL;
+
+const get = (endPoint, urlParam, requestInit = {}) => {
+  return fetch(
+    API_URL + '/' + endPoint + '?' + new URLSearchParams(urlParam).toString(),
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      ...requestInit,
+    },
+  );
+};
+(async () => {
+  const userGetResponse = await get('users');
+  const user = await userGetResponse.json();
+  console.log(user);
+})();
