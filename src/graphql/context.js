5f28eceabac60c3c3fdb6163f9a8854a30c5c4d8
Márcia Nardes <marcianarddes@gmail.com>
2022-10-11 11:29:52 -0300

    aula 59

---
+++ src/graphql/context.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+const authorizeUser = (req) => {
+  // req.readers.authorization
+  const { headers } = req;   // requisição
+  const { authorization } = headers;
+
+  try {
+    const [_bearer, token] = authorization.split('');
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET); // verifica se o token é válido
+    return userId;
+  }catch (e) { // (e) se apresentar erro, dá uma string vazia
+    return '';
+  }
+};
+
+  // Authorization: Bearer e a chave token
+  // na função abaixo valida se o usuário está logado ou não
+export const context = ({ req }) => {
+  const loggedUserId = authorizeUser(req);
+
+  return {
+    loggedUserId,
+  };
+};
