e5378928ef19e2f7d8cd879259f17adc17015cda
Márcia Nardes <marcianarddes@gmail.com>
2022-10-17 11:29:33 -0300

    teste

--- src/graphql/login/login-functions.js
+++ src/graphql/login/login-functions.js
@@ -3,12 +3,14 @@ import { AuthenticationError } from 'apollo-server-errors';
 export const checkIsLoggedIn = (loggedUserId) => {
   if (!loggedUserId) {
     throw new AuthenticationError('You have to log in');
- }
-};
-export const checkOwner = (userId, loggedUserId) => {
-  checkIsLoggedIn(loggedUserId);
-  if (loggedUserId !== userId) {
-    throw new AuthenticationError('You cannot update this user');
   }
 };
+export const checkOwner = (userId, loggedUserId) => {
+
+  checkIsLoggedIn(loggedUserId);
+  if (loggedUserId === userId) {
+    throw new AuthenticationError('You cannot update this user');
+    console.log(loggedUserId)};
+  };
+
 
