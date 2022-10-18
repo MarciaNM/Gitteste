93b679ee213439864a7c5d52058ecc3cc7e5beb2
Márcia Nardes <marcianarddes@gmail.com>
2022-10-18 10:14:22 -0300

    Co-authored-by: cadumielkezim@outlook.com <cadumielkezim@outlook.com>

--- src/graphql/login/resolvers.js
+++ src/graphql/login/resolvers.js
@@ -1,6 +1,6 @@
 export const login = async (_, { data }, { dataSources }) => {
   const { userName, password } = data;
-  return dataSources.LoginApi.login(userName, password);
+  return dataSources.loginApi.login(userName, password);
   //return dataSources.loginApi.login(userName, password);antes
 };
 
