a11992d7d45bf897f56921f9aa208c7211fc21a2
Márcia Nardes <marcianarddes@gmail.com>
2022-10-18 10:09:40 -0300

    testes

--- src/graphql/login/resolvers.js
+++ src/graphql/login/resolvers.js
@@ -1,6 +1,7 @@
-export const login = async(_, { data}, {dataSources}) => {
-  const {userName, password} = data;
-  return dataSources.loginApi.login(userName, password);
+export const login = async (_, { data }, { dataSources }) => {
+  const { userName, password } = data;
+  return dataSources.LoginApi.login(userName, password);
+  //return dataSources.loginApi.login(userName, password);antes
 };
 
 export const loginResolvers = {
