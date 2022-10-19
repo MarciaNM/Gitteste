407bc904f517615b476729e138441d7fc687e703
Márcia Nardes <marcianarddes@gmail.com>
2022-10-19 09:49:23 -0300

    alteração do arquivo post-repository

--- src/graphql/post/utils/post-repository.js
+++ src/graphql/post/utils/post-repository.js
@@ -3,36 +3,37 @@ import { FetchError } from 'node-fetch';
 
 export const createPostFn = async (postData, DataSource) => {
   const postInfo = await createPostInfo(postData, DataSource);
-  const { userId } = postInfo;
-  const { title, body } = postInfo;
-
-  console.log(postInfo);
+  const { title, body, userId } = postInfo;
 
   if (!title || !body || !userId) {
-    throw new ValidationError('you have to send title, body e userId');
+    throw new ValidationError('You have to send title, body and userId');
   }
+
   return await DataSource.post('', { ...postInfo });
 };
 
-//criação do post update aula 47
+export const findPostOwner = async (postId, DataSource) => {
+  const foundPost = await DataSource.get(postId, undefined, {
+    cacheOptions: { ttl: 0 },
+  });
+
+  if (!foundPost) {
+    throw new FetchError('Could not find the post you are looking for.');
+  }
+
+  if (foundPost.userId !== dataSource.context.loggedUserId) {
+    throw new AuthenticationError('You cannot update this post 😠!');
+  }
+
+  return foundPost;
+};
+
 export const updatePostFn = async (postId, postData, DataSource) => {
   if (!postId) {
     throw new ValidationError('Missing postId');
   }
-  const foundPost = await DataSource.get(postId, undefined, { // aula 66
-    cacheOptions: { ttl: 0 },
-  });
 
-  if (!foundPost) { // aula 66
-    throw new FetchError('Could not find the post you are looking for.');
-  }
-  if (foundPost.userId !== DataSource.context.loggedUserId) { // aula 66
-    throw new AuthenticationError('You cannot update this post!');
-  }
-
-  console.log(foundPost);
-
-  const { userId } = foundPost; // aula 66
+  const { userId } = await findPostOwner(postId, DataSource);
   const { title, body } = postData;
 
   if (typeof title !== 'undefined') {
@@ -40,30 +41,34 @@ export const updatePostFn = async (postId, postData, DataSource) => {
       throw new ValidationError('title missing');
     }
   }
+
   if (typeof body !== 'undefined') {
     if (!body) {
       throw new ValidationError('body missing');
     }
   }
+
   if (typeof userId !== 'undefined') {
     if (!userId) {
       throw new ValidationError('userId missing');
     }
-    await userExists(userId, DataSource);
+    await userExists(userId, dataSource);
   }
-  return DataSource.patch(postId, { ...postData });
+
+  return dataSource.patch(postId, { ...postData });
 };
-// aula 48
+
 export const deletePostFn = async (postId, DataSource) => {
   if (!postId) throw new ValidationError('Missing postId');
+  await findPostOwner(postId, DataSource);
 
   const deleted = await DataSource.delete(postId);
-  return !!deleted; // !! converte para boolean o delete
+  return !!deleted;
 };
 
 const userExists = async (userId, DataSource) => {
   try {
-    console.log(DataSource.context.dataSources)
+    await DataSource.context.dataSources.userApi.get(userId);
   } catch (e) {
     throw new ValidationError(`User ${userId} does not exist`);
   }
@@ -77,7 +82,7 @@ const createPostInfo = async (postData, DataSource) => {
   const indexRefPost = await DataSource.get('', {
     _limit: 1,
     _sort: 'indexRef',
-    _order: 'desc'
+    _order: 'desc',
   });
 
   const indexRef = indexRefPost[0].indexRef + 1;
