184916e7e77f4aac6ae5fbf274b0b7dfe6b023a2
MarciaNM <57048301+MarciaNM@users.noreply.github.com>
2022-11-14 18:02:01 -0300

    teste do docker

---
+++ src/knex/migrations/20221110145033_create-comments-table.js
@@ -0,0 +1,16 @@
+
+exports.up = async function(knex) {
+  return knex.schema.createTable('comments', (table) => {
+    table.increments('id').primary();
+    table.text('comments').notNullable();
+    table.string('post_id',255).notNullable();
+    table.string('user_id',255).notNullable();
+    table.timestamps(true, true);
+
+  });
+};
+
+exports.down = async function(knex) {
+  return knex.schema.dropTable('comments');
+  
+};
